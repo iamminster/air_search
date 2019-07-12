8538d56df467fd1140ec679b18fb9bdcc863bf3d
--get <nguyenminh@1463-nguyen.local>
2019-07-12 15:24:54 +0900

    First commit

---
+++ src/main/java/minh/nguyen/eas/air/AirSearchApplication.java
@@ -0,0 +1,13 @@
+package minh.nguyen.eas.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirSearchApplication.class, args);
+	}
+
+}
