33c41e71dd43e457d6e9599f57e46498acd26d6f
nguyen.minh@airtrip-corp.com <nguyenminh@ip-172-20-30-1.ap-northeast-1.compute.internal>
2019-09-24 13:09:56 +0900

    Add search controller

--- src/main/java/minh/nguyen/eas/air/search/AirSearchApplication.java
+++ src/main/java/minh/nguyen/eas/air/search/AirSearchApplication.java
@@ -1,4 +1,4 @@
-package minh.nguyen.eas.air;
+package minh.nguyen.eas.air.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/minh/nguyen/eas/air/search/controller/SearchController.java
@@ -0,0 +1,9 @@
+package minh.nguyen.eas.air.search.controller;
+
+public class SearchController {
+
+	public SearchController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
